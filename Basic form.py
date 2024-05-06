7af99f95faa53e03952d65e21f2a7ec19ef0c47d
Leoneix <168160706+Leoneix@users.noreply.github.com>
2024-05-06 13:31:42 +0530

    Update Basic form.py

--- Basic form.py
+++ Basic form.py
@@ -1,6 +1,11 @@
+# Last updated on 06 May 24
+
 varName=input("Your name:") 
 varOrg=input("Origin IATA code:")
 varDes=input("Destination IATA code:")
 varDate=input("Date in the format DDMMYY:")
 
-print("Ticket Confirmed! \nYour PNR is:", varDes[0:2],varDate[1:2],varOrg[0:3],varName[-2:-1] )
+print("Booking Confirmed! \nYour PNR is:", varDes[0:2],varDate[1:2],varOrg[0:3],varName[-2:-1] )
+print( "Name:", varName.upper() )
+print( "Destination:", varDes.upper() )
+print( "Date:", varDate.upper() )
